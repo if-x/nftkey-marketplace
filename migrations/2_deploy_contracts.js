e7d375681f6a7ed73d94836ae09328500a49a0dc
if-x <ifx.creator@gmail.com>
2021-04-01 12:08:14 +1100

    Deployed for BMoonCat

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,13 +7,22 @@ module.exports = async (deployer, network
 // accounts: string[]
 ) => {
     console.log(network);
-    await deployer.deploy(TestERC721);
-    const erc721 = await TestERC721.deployed();
-    console.log(`TestERC721 deployed at ${erc721.address} in network: ${network}.`);
-    await deployer.deploy(TestERC20);
-    const erc20 = await TestERC20.deployed();
-    console.log(`TestERC20 deployed at ${erc20.address} in network: ${network}.`);
-    await deployer.deploy(NFTKEYMarketPlaceV1, "Test ERC721", erc721.address, erc20.address);
-    const marketplaceV1 = await NFTKEYMarketPlaceV1.deployed();
-    console.log(`NFTKEYMarketPlaceV1 deployed at ${marketplaceV1.address} in network: ${network}.`);
+    if (network === "development") {
+        await deployer.deploy(TestERC721);
+        const erc721 = await TestERC721.deployed();
+        console.log(`TestERC721 deployed at ${erc721.address} in network: ${network}.`);
+        await deployer.deploy(TestERC20);
+        const erc20 = await TestERC20.deployed();
+        console.log(`TestERC20 deployed at ${erc20.address} in network: ${network}.`);
+        await deployer.deploy(NFTKEYMarketPlaceV1, "Test ERC721", erc721.address, erc20.address);
+        const marketplaceV1 = await NFTKEYMarketPlaceV1.deployed();
+        console.log(`NFTKEYMarketPlaceV1 deployed at ${marketplaceV1.address} in network: ${network}.`);
+    }
+    if (network === "bsc") {
+        await deployer.deploy(NFTKEYMarketPlaceV1, "BMoonCats", "0x7A00B19eDc00fa5fB65F32B4D263CE753Df8f651", // BMoonCats
+        "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" // WBNB
+        );
+        const marketplaceV1 = await NFTKEYMarketPlaceV1.deployed();
+        console.log(`NFTKEYMarketPlaceV1 for BMoonCats deployed at ${marketplaceV1.address} in network: ${network}.`);
+    }
 };
